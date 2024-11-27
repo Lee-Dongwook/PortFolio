c89cdf8fbf2f5e2016f69b692c80ab65e8e35a11
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-11-27 16:13:12 +0900

    feat : About 페이지 초안

---
+++ src/app/about/page.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import AboutTemplate from "@/components/template/about/AboutTemplate";
+
+export default function AboutPage() {
+  return <AboutTemplate />;
+}
---
+++ src/components/template/about/AboutTemplate.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import AboutOrganism from "@/components/organisms/about/AboutOrganism";
+
+const AboutTemplate = () => {
+  return <AboutOrganism />;
+};
+export default AboutTemplate;
