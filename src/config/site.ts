c233f942fae1c531b4f9d90d63c6ec9825d33295
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-11-28 11:20:31 +0900

    feat : Main Nav 및 Mobile Nav 작성

---
+++ src/config/site.ts
@@ -0,0 +1,15 @@
+export const siteConfig = {
+  name: "Lee DongWook\'s PortFolio",
+  authorName: "Lee DongWook",
+  username: "leedongwook",
+  description:
+    "향상된 사용자 경험 제공을 목표하는 1년차 프론트엔드 개발자입니다.",
+  url: "",
+  links: {
+    github: "",
+  },
+  ogImage: "",
+  iconIco: "",
+  logoIcon: "",
+  keywords: ["Lee Dong Wook", "Lee Dong Wook\'s PortFolio"],
+};
