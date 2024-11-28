91bcacb8f7287242ed3186df67e75341bfffc234
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-11-28 10:19:17 +0900

    feat : More Shadcn Component

---
+++ src/config/constants.ts
@@ -0,0 +1,21 @@
+export type ValidSkills =
+  | "React"
+  | "Next.js"
+  | "JavaScript"
+  | "TypeScript"
+  | "HTML5"
+  | "CSS3"
+  | "Redux-Toolkit"
+  | "Zustand"
+  | "Socket.io"
+  | "Tailwind CSS"
+  | "Shadcn ui"
+  | "Node.js"
+  | "Express"
+  | "StoryBook"
+  | "Playwright"
+  | "MongoDB"
+  | "AWS S3"
+  | "AWS CloudFront";
+
+export type ValidCategory = "Frontend Developer";
