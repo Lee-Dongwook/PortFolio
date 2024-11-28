b0df8c73fab01a005eb267ed47e924421680d496
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-11-28 17:54:38 +0900

    feat : Experience page

---
+++ src/app/experience/page.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ExperienceTemplate from "@/components/template/experience/ExperienceTemplate";
+
+export default function ExperiencePage() {
+  return <ExperienceTemplate />;
+}
---
+++ src/components/template/experience/ExperienceTemplate.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ExperienceOrganism from "@/components/organisms/experience/ExperienceOrganism";
+
+const ExperienceTemplate = () => {
+  return <ExperienceOrganism />;
+};
+
+export default ExperienceTemplate;
