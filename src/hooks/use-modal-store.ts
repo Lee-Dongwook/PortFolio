dd1f181c24d5b4a22e1517dcc97a753cd0fbabb8
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-11-28 10:16:22 +0900

    feat : Modal

---
+++ src/hooks/use-modal-store.ts
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { create } from "zustand";
+
+interface ModalDataProps {
+  title: string;
+  description: string;
+  icon: any;
+}
+
+interface ModalStoreProps {
+  isOpen: boolean;
+  title: string;
+  description: string;
+  icon: any;
+  onOpen: (data: ModalDataProps) => void;
+  onClose: () => void;
+}
+
+export const useModalStore = create<ModalStoreProps>((set) => ({
+  isOpen: false,
+  title: "",
+  description: "",
+  icon: null,
+  onOpen: (data: any) =>
+    set({
+      isOpen: true,
+      title: data.title,
+      description: data.description,
+      icon: data.icon,
+    }),
+  onClose: () => set({ isOpen: false }),
+}));
